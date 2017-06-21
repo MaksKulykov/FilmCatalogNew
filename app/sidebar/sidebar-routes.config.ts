e8e652410b3c3b282a620917533660848207d468
MaksKulykov <kulykov.maks@gmail.com>
2017-06-21 09:50:05 +0300

    renamed sidebar items

--- app/sidebar/sidebar-routes.config.ts
+++ app/sidebar/sidebar-routes.config.ts
@@ -1,7 +1,7 @@
 import {  RouteInfo } from './sidebar.metadata';
 
 export const ROUTES: RouteInfo[] = [
-    { path: 'popular', title: 'Popular Films',  icon: 'theaters' },
-    { path: 'details', title: 'Film Details',  icon:'description' },
-    { path: 'favorite', title: 'Favorite Films',  icon:'favorite' },
+    { path: 'popular', title: 'Popular Movies',  icon: 'theaters' },
+    { path: 'details', title: 'Movie Details',  icon:'description' },
+    { path: 'favorite', title: 'Favorite Movies',  icon:'favorite' },
 ];
