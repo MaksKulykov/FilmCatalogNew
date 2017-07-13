487cafa9d3fa9755ffbaf73e8478903718944633
MaksKulikov <kulykov.maks@gmail.com>
2017-07-13 19:54:39 +0300

    formated code

--- app/dashboard/film.service.ts
+++ app/dashboard/film.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { Http, Response, URLSearchParams } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Http, Response, URLSearchParams} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
@@ -11,7 +11,9 @@ export class FilmService {
     private apiKey: string = '0077bec633f957fbf7cd91c9c7026b27';
     private language: string = 'en-US';
     private pageNumber: string = '1';
-    constructor(private http: Http) { }
+
+    constructor(private http: Http) {
+    }
 
     private extractListData(res: Response): Array<any> {
         let body = res.json();
@@ -34,7 +36,9 @@ export class FilmService {
         params.set('language', this.language);
         params.set('page', this.pageNumber);
         return this.http.get(this.url + 'popular', {search: params}).map(this.extractListData)
-            .catch((error: any)=> { return Observable.throw(error);});
+            .catch((error: any) => {
+                return Observable.throw(error);
+            });
     }
 
     getFilmDetails(id: string): Observable<any> {
@@ -42,14 +46,18 @@ export class FilmService {
         params.set('api_key', this.apiKey);
         params.set('language', this.language);
         return this.http.get(this.url + id, {search: params}).map(this.extractDetailsData)
-            .catch((error: any)=> { return Observable.throw(error);});
+            .catch((error: any) => {
+                return Observable.throw(error);
+            });
     }
 
     getActors(id: string): Observable<any> {
         let params: URLSearchParams = new URLSearchParams();
         params.set('api_key', this.apiKey);
         return this.http.get(this.url + id + '/credits', {search: params}).map(this.extractListActors)
-            .catch((error: any)=> { return Observable.throw(error);});
+            .catch((error: any) => {
+                return Observable.throw(error);
+            });
     }
 
 }
